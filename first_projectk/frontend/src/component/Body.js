518175c5b632483fed2428561b4fa5809839d91a
prathmesh703 <146568950+prathmesh703@users.noreply.github.com>
2024-08-03 19:43:01 +0530

    stocks page added

--- first_projectk/frontend/src/component/Body.js
+++ first_projectk/frontend/src/component/Body.js
@@ -1,22 +1,32 @@
 import React from "react";
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import Register from "./Register.js"
-import Browse from "./Browse.js"
-const Body =()=>{
-    const appRouter = createBrowserRouter([
-        {
-            path:'/',
-            element:<Register/>
-        },
-        {
-            path:'/browse',
-            element:<Browse/>
-        }
-    ])
-    return(
-        <div>
-            <RouterProvider router={appRouter}/>
-        </div>
-    )
-}
-export default Body
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./Register.js";
+import Browse from "./Browse.js";
+import Home from "./home.js";
+import Stocks from "./stocks/stocks.js";
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/stocks",
+      element: <Stocks />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+export default Body;
